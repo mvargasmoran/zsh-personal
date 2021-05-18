c517ef0dcebd142cdaa3b96011504971f578299c
mvargasmoran <mvargasmoran@gmail.com>
2021-05-18 02:37:41 -0400

    Add an install tool
    This should just
    - Backup whatever active zsh configuration
    - Clone the "fended" repository
    - Create symlinks to home directory
    
    Then just firing up a new terminal session (new tab/new window) should
    source and be ready to use, you just have Ended your Friendship with
    plugin managers.

---
+++ tools/install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# This should be run via curl
+#
+# or just download, navigate to the directory and do:
+#   sh install.sh
+#
+
+# move the .zshrc and ~/.config/zsh-personal directory to ~/old-zsh directory
+mkdir ~/old-zsh
+
+mv ~/.zshrc ~/old-zsh/.zshrc
+mv ~/.zshenv ~/old-zsh/.zshenv
+mv ~/.zsh_history ~/old-zsh/.zsh_history
+mv ~/.zsh-update ~/old-zsh/.zsh-update
+
+mkdir ~/.config/fended
+cd ~/.config/fended
+
+git clone git@github.com:mvargasmoran/zsh-personal.git .
+cd ~/.config/fended
+
+mv ~/.config/fended/.zshrc ~/.zshrc
+mv ~/.config/fended/.zshenv ~/.zshenv
+
+
+fendedDirectory='~/.config/fended/'
+ln -s ~/.config/fended/.zshrc
+ln -s ~/.config/fended/.zshenv
+
